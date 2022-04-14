52118161de0f7de4fc734a90f26c86e371b60d82
R. Tinkle <rtinkle.dev@gmail.com>
2022-04-14 08:32:48 -0400

    Mods

--- app/root.jsx
+++ app/root.jsx
@@ -5,13 +5,58 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  //ErrorBoundary,
+  //CatchBoundary
+  useCatch
 } from "@remix-run/react";
 
 export const meta = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Remix App",
   viewport: "width=device-width,initial-scale=1",
-});
+})
+
+export function ErrorBoundary({ error }) {
+  return (
+    <div>
+      <h1>Error Boundary</h1>
+      <p>{error.message}</p>
+      <p>The stack trace is:</p>
+      <pre>{error.stack}</pre>
+    </div>
+  )
+}
+
+export function CatchBoundary() {
+  let caught = useCatch()
+  console.log('in CatchBoundary - caught', caught)
+  let message
+  switch (caught.status) {
+    case 401:
+      message = (
+        <p>
+          Oops! Looks like you tried to visit a page that you do not have access
+          to.
+        </p>
+      );
+      break;
+    case 404:
+      message = (
+        <p>Oops! Looks like you tried to visit a page that does not exist.</p>
+      )
+      break
+
+    default:
+      throw new Error(caught.data || caught.statusText)
+  }
+  return (
+    <>
+      <h1>Response Catch Boundary</h1>
+      {message}
+      {caught.status}
+    </>
+  )
+}
 
 export default function App() {
   return (
@@ -27,5 +72,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
